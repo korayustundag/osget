1f874d24c52b62e78a8a1c1def6d65c0316470c7
Koray Üstündağ <mskorayustundag@gmail.com>
2023-04-01 19:26:04 +0300

    v1.0.0

---
+++ src/osget/App.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace osget
+{
+    internal static class App
+    {
+        internal static List<string> OsIds = new List<string>();
+        internal static List<string> OsNames = new List<string>();
+        internal static List<string> OsFileSize = new List<string>();
+        internal static List<string> OsDownloadUrl = new List<string>();
+
+        internal static void Exit(int exitCode)
+        {
+            Console.Write("Press enter to exit...");
+            Console.ReadLine();
+            Environment.Exit(exitCode);
+        }
+
+        internal static void ShowHelp()
+        {
+            Console.WriteLine("Argument List:");
+            Console.WriteLine("  -h or --help    :: Showing Help");
+            Console.WriteLine("  -v or --version :: Showing App Version Information");
+            Console.WriteLine("  -i or --install :: Install Command");
+            Console.WriteLine("  -l or --list    :: Showing Installable Operating Systems");
+            Console.WriteLine("=====");
+            Console.WriteLine("Usage:");
+            if (OperatingSystem.IsWindows())
+            {
+                Console.WriteLine("osget.exe [osname] [path]");
+                Console.WriteLine("Example 1: osget.exe -i debian C:\\folder\\debian.iso");
+                Console.WriteLine("Example 2: osget.exe --install debian \"C:\\my folder\\debian.iso\"");
+                Console.WriteLine("Example 3: osget.exe -i debian debian.iso");
+            }
+            else
+            {
+                Console.WriteLine("./osget [osname] [path]");
+                Console.WriteLine("Example 1: ./osget -i debian /home/user/folder/debian.iso");
+                Console.WriteLine("Example 2: ./osget --install debian \"/home/user/my folder/debian.iso\"");
+                Console.WriteLine("Example 3: ./osget -i debian debian.iso");
+            }
+            Console.WriteLine("=====");
+            Environment.Exit(0);
+        }
+
+        internal static void ShowVersion()
+        {
+            Console.WriteLine("Welcome to OsGet");
+            Console.WriteLine("Version  : 1.0.0");
+            Console.WriteLine("Source   : https://github.com/korayustundag/osget");
+            Environment.Exit(0);
+        }
+
+        internal static void ShowError(string errorMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorMessage);
+            Console.ResetColor();
+        }
+
+        internal static void CheckNetworkConnection()
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply result = ping.Send("www.githubusercontent.com");
+                if (result.Status != IPStatus.Success)
+                {
+                    ShowError("Error: Could not connect to server! Please check your internet connection.");
+                    Exit(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while connecting to the server:: {ex.Message}");
+                Exit(1);
+            }
+        }
+
+    }
+}
---
+++ src/osget/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace osget
+{
+    internal class Program
+    {
+        public static string inputOsName { get; set; }
+        public static string inputSavePath { get; set; }
+
+        const string githubUrl = "https://raw.githubusercontent.com/korayustundag/osget/main/list/oslist.txt";
+
+        static async Task Main(string[] args)
+        {
+            Console.Title = "OsGet";
+            if (!args.Any())
+            {
+                App.ShowError("Argument Error! Use \"--help\" command");
+                App.Exit(1);
+            }
+            App.CheckNetworkConnection();
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(githubUrl))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string fullList = await content.ReadAsStringAsync();
+                        string[] lines = fullList.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (string line in lines)
+                        {
+                            string[] data = line.Split(',');
+                            App.OsIds.Add(data[0]);
+                            App.OsNames.Add(data[1]);
+                            App.OsFileSize.Add(data[2]);
+                            App.OsDownloadUrl.Add(data[3]);
+                        }
+                    }
+                }
+            }
+
+            string argument = args[0];
+            if (string.Equals(argument, "-h") || string.Equals(argument, "--help"))
+            {
+                App.ShowHelp();
+            }
+            else if (string.Equals(argument, "-v") || string.Equals(argument, "--version"))
+            {
+                App.ShowVersion();
+            }
+            else if (string.Equals(argument, "-l") || string.Equals(argument, "--list"))
+            {
+                Console.WriteLine("===OS LIST=======");
+                for (int i = 0; i < App.OsNames.Count(); i++)
+                {
+                    Console.WriteLine();
+                    string name = "----" + App.OsNames[i] + "----";
+                    Console.WriteLine(name);
+                    Console.WriteLine("Name: " + App.OsIds[i]);
+                    Console.WriteLine("File Size: " + App.OsFileSize[i]);
+                    for (int z = 0; z < name.Length; z++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("=================");
+                App.Exit(0);
+            }
+            else if (string.Equals(argument, "-i") || string.Equals(argument, "--install") || string.Equals(argument, "install"))
+            {
+                if (args.Count() > 1)
+                {
+                    inputOsName = args[1];
+                    if (args.Count() > 2)
+                    {
+                        inputSavePath = args[2];
+                    }
+                    else
+                    {
+                        App.ShowError("Argument Error: Path");
+                        App.Exit(1);
+                    }
+                }
+                else
+                {
+                    App.ShowError("Argument Error: Os Name");
+                    App.Exit(1);
+                }
+            }
+            else
+            {
+                App.ShowError("Error: Unknown Command => " + argument);
+                App.Exit(1);
+            }
+
+            Console.WriteLine("Welcome to OsGet");
+            
+
+            bool isMatch = false;
+            int count = 0;
+
+            foreach (string osid in App.OsIds)
+            {
+                if (osid == inputOsName)
+                {
+                    isMatch = true;
+                    break;
+                }
+                count++;
+            }
+
+            if (!isMatch)
+            {
+                App.ShowError("Error: Invalid OS Name => "+inputOsName);
+                App.ShowError("Use \"-l\" or \"--list\" command");
+                App.Exit(1);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("===OS INFO=======");
+            Console.WriteLine("Selected OS:" + App.OsNames[count]);
+            Console.WriteLine("Total File Size:" + App.OsFileSize[count]);
+            Console.WriteLine("=================");
+            Console.WriteLine();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                App.ShowError("Error: " + ex.Message);
+                throw;
+            }
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(App.OsDownloadUrl[count], HttpCompletionOption.ResponseHeadersRead);
+                var contentLength = response.Content.Headers.ContentLength ?? -1L;
+                var buffer = new byte[81920];
+                var bytesRead = 0L;
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var fileStream = File.Create(inputSavePath))
+                {
+                    var read = 0;
+                    while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await fileStream.WriteAsync(buffer, 0, read);
+                        bytesRead += read;
+                        var progress = (int)(100 * bytesRead / contentLength);
+                        Console.Write($"\r[{new string('#', progress / 2)}{new string('-', 50 - progress / 2)}] {progress}%");
+                    }
+                }
+            }
+            Console.WriteLine(" Download completed!");
+            App.Exit(0);
+        }
+    }
+}
